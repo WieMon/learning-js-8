77eed09445fb6c958ccf1fd0257cc88a2499e335
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-06-04 19:01:15 +0200

    Add the effect of typing

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const spnText = document.querySelector('.text');
+const txt = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore tenetur rerum ad? Eum quos reprehenderit minima! Fugit, omnis! Eveniet repellendus et iure deserunt iusto quas in est odit, labore optio? THE END';
+
+let indexText = 0;
+const time = 40;
+
+const addLetter = () => {
+  spnText.textContent += txt[indexText];
+  indexText++;
+  if (indexText === txt.length) clearInterval(indexTyping);
+ }
+ const indexTyping = setInterval(addLetter, time);
